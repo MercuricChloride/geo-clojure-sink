6aa42f56afe2c9d7ad0f9a5f1fe9e8b2e28f2f43
mercuricchloride <mercuryprotocol@protonmail.com>
2023-10-23 02:29:19 -0700

    Dumping millions of entries in a few seconds lol

--- project.clj
+++ project.clj
@@ -3,7 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"][cheshire "5.12.0"]]
+  :dependencies [
+                  [org.clojure/clojure "1.10.0"]
+                  [cheshire "5.12.0"]
+                  [com.github.seancorfield/next.jdbc "1.3.894"]
+                  [org.postgresql/postgresql "42.6.0"]
+                  [com.github.seancorfield/honeysql "2.4.1078"]
+                  [com.zaxxer/HikariCP "5.0.1"]]
   :main ^:skip-aot lein-test.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/lein_test/core.clj
+++ src/lein_test/core.clj
@@ -3,35 +3,60 @@
   (:require
    [cheshire.core :as ch]
    [clojure.spec.alpha :as s]
+   [clojure.string :as cstr]
    [clojure.core.reducers :as r]
+   [clojure.java.io :as io]
    [lein-test.spec.action :as action]
-   [lein-test.spec.action-type :as at]))
+   [honey.sql :as sql]
+   [honey.sql.helpers :as h]
+   [next.jdbc :as jdbc]
+   [next.jdbc.connection :as connection]
+   [lein-test.spec.action-type :as at]
+   [lein-test.constants :refer [ROOT-SPACE-ADDRESS]])
+  (:import (com.zaxxer.hikari HikariDataSource)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
 
+(def ds (connection/->pool com.zaxxer.hikari.HikariDataSource
+                   {:dbtype "postgres" :dbname "geo-global" :username "postgres" :password "fart" :maximumPoolSize 10
+                    :dataSourceProperties {:socketTimeout 30}}))
+;; (def pg-db {:dbtype "postgresql"
+;;             :dbname "geo-global"
+;;             :host "localhost"
+;;             :user "postgres"
+;;             :password "fart"})
+
+;(def ds (jdbc/get-datasource datasource-options))
+
+
+(defn action->struct [action]
+  (let [type (:type action) entityId (:entityId action) attributeId (:attributeId action)]
+    {
+   :id (str type "-" entityId "-" attributeId)
+   :action_type type
+   :entity entityId
+   :attribute attributeId
+   }))
+
+(defn action->entity [action]
+  (let [entityId (:entityId action)]
+    {:id entityId}))
+
 (defn- validate-actions [actions]
   (filter #(action/valid-action? %) actions))
 
-(defn- json->actions [path]
+(defn- json->actions
+ ([path]
   (let [json (slurp path)]
     (validate-actions ((ch/parse-string json true) :actions))))
+ ([prefix path]
+  (let [json (slurp (str prefix path))]
+    (validate-actions ((ch/parse-string json true) :actions)))))
 
-(def actions (json->actions "geo-data.json"))
-
-(defn unique-entities
-  "Returns a hash set of unique entities from a collection of maps"
-  ([coll key] (reduce #(unique-entities %1 %2 key) #{} coll))  ; if a collection is provided, reduce over it
-  ([acc curr key] (conj acc (key curr))))   ; if two arguments are provided, assume it's the reducing step
-
-(def valid-actions (validate-actions actions))
-
-(defn repeat-vector [v n]
-  (vec (take n (cycle v))))
-
-(def long-actions (repeat-vector valid-actions 2000))
+(def actions (validate-actions (json->actions "geo-data.json")))
 
 (defn retry-future
   "Tries to execute a future n times, backing for 5 seconds longer each time it returns false"
@@ -54,8 +79,62 @@
       (try3 dependencies)
       @handler)))
 
-(time (doall (map #(deref % 5000 "timed out") (map #(handle-execute (at/execute %)) (filter #(at/sink-action %) long-actions)))))
+;(time (doall (map #(deref % 5000 "timed out") (map #(handle-execute (at/execute %)) (filter #(at/sink-action %) long-actions)))))
 
-(def unique-actions (concat
-                     (unique-entities valid-actions :entityId)
-                     (unique-entities valid-actions :attributeId)))
+
+;(println (time (jdbc/execute! ds query-str)))
+;(jdbc/execute! ds nuke-actions)
+
+(def nuke-actions (-> (h/delete-from :actions)
+                      (h/where [:= :value-type nil])
+                      (sql/format {:pretty true})))
+
+(def nuke-entities (-> (h/delete-from :entities)
+                      (h/where [:= :value-type nil])
+                      (sql/format {:pretty true})))
+(jdbc/execute! ds nuke-actions)
+(jdbc/execute! ds nuke-entities)
+
+(defn query-str [actions]
+(-> (h/insert-into :actions)
+    (h/values (into [] (map action->struct actions)))
+    (h/on-conflict :id (h/do-nothing))
+    (sql/format {:pretty true})))
+
+(defn entity-query [actions]
+(-> (h/insert-into :entities)
+    (h/values (into [] (map action->entity actions)))
+    (h/on-conflict :id (h/do-nothing))
+    (sql/format {:pretty true})))
+
+(defn entity-exists [entity-id]
+  (-> (h/select :*)
+      (h/from :entities)
+      (h/where [:= :id entity-id])
+      (sql/format {:pretty true})))
+
+(defn table-key [space address]
+  (keyword space address))
+
+(table-key ROOT-SPACE-ADDRESS "foo")
+
+(sql/format {:alter-table :public/entities :add-column [:id :text [:not nil] :if-not-exists]})
+
+(defn entity->table [entity]
+  (keyword (:entities/defined-in entity) (:entities/id entity)))
+
+(def files (->> (io/file "./action_cache/")
+               file-seq
+               rest
+               (map #(cstr/replace % #"./action_cache/" ""))
+               (sort-by #(get (cstr/split % #"_" ) 0))
+               (map #(json->actions "./action_cache/" %))))
+
+
+(defn try-execute [query]
+  (try
+    (jdbc/execute! ds query)
+    (catch Exception e
+      (println e "failed to insert entity" query))))
+
+(time (map #(do (try-execute (entity-query %))(try-execute (query-str %))) files))
