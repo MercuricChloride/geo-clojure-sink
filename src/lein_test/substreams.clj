4b2d61cf67839bc282562e4ca8c5949015888c2e
mercuricchloride <mercuryprotocol@protonmail.com>
2023-10-31 11:35:58 -0700

    Substream workin

--- src/lein_test/substreams.clj
+++ src/lein_test/substreams.clj
@@ -4,10 +4,20 @@
             [sf.substreams.rpc.v2.Stream.client :as stream]
             [protojure.grpc.client.providers.http2 :as grpc.http2]
             [com.example.addressbook.Greeter.client :as greeter]
+            [geo.clojure.sink :as geo]
             [dotenv :refer [env app-env]]
             [clojure.core.async :as async]
             [clojure.java.io]
             [sf.substreams.v1 :as v1]))
+
+(defn take-all [ch f]
+  (async/go (loop []
+        (let [val (async/<! ch)]
+          (if val
+            (do
+              (f val)
+              (recur))
+            (println "Channel closed"))))))
 
 (defn slurp-bytes
   "Slurp the bytes from a slurpable thing"
@@ -17,31 +27,37 @@
     (clojure.java.io/copy in out)
     (.toByteArray out)))
 
+(defn handle-block-scoped-data
+  [data channel]
+  (let [message (:message data)
+        block-data (:block-scoped-data message)
+        output (:output block-data)
+        map-output (:map-output output)
+        map-value (:value map-output)]
+    (try
+      (let [entries-added (geo/pb->EntriesAdded map-value)]
+        (when (not (= 0 (count (:entries entries-added))))
+          (async/put! channel entries-added)))
+      (catch Exception e
+        ;; (println "Error parsing map output:" e)
+        ))))
+
 (def spkg (v1/pb->Package (slurp-bytes "substream.spkg")))
-
-;(def client @(grpc.http2/connect {:uri "http://localhost:8080"}))
-                                  ;:metadata ["foo" "bar"]}))
-
-;@(greeter/Hello client {:name "Janet Johnathan Doe"})
 
 (def client @(grpc.http2/connect {:uri "https://polygon.substreams.pinax.network:443"
                                   :ssl true
                                   :metadata {"authorization" (env "SUBSTREAMS_API_TOKEN")}}))
 
-@(stream/Blocks client (rpc/new-Request {}) (async/chan 100))
-  ;; (:gen-class)
-  ;; (:require
-  ;;  [cheshire.core :as ch]
-  ;;  [lein-test.constants :refer [ENTITIES]]
-  ;;  [clojure.string :as cstr]
-  ;;  [clojure.java.io :as io]
-  ;;  [lein-test.spec.action :as action]
-  ;;  [honey.sql :as sql]
-  ;;  [honey.sql.helpers :as h]
-  ;;  [next.jdbc :as jdbc]
-  ;;  [lein-test.tables :refer [->action ->triple ->entity ->entity-type ->entity-attribute ->spaces]]
-  ;;  [lein-test.db-helpers :refer [nuke-db try-execute create-type-tables make-space-schemas]
-  ;;  [sf.substreams.rpc.v2 :as rpc]]))
+(let [channel (async/chan) output-channel (async/chan)]
+  ; we start a thread to initiate the stream
+  (async/thread (stream/Blocks client (rpc/new-Request {:start-block-num 36472424
+                                          :stop-block-num 36500000
+                                          :modules (:modules spkg)
+                                          :output-module "map_entries_added"}) channel))
+ ; we then start another thread to filter out the entries that have data
+  (async/thread
+    (take-all channel #(handle-block-scoped-data % output-channel)))
 
-;; Our grpc connection must pass in the authorization key with the value of our "token" r.metadata_mut().insert("authorization", t.clone());
-;; :headers {"Content-Type" "application/grpc-web-text"}
+ ; we then start yet another thread to "process" the entries
+  (async/thread
+    (take-all output-channel println)))
