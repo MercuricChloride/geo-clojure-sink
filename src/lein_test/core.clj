9f1671b030d0804cab71c9339cc06fd039a023d4
badgeth-dao <81835284+badgeth-dao@users.noreply.github.com>
2023-11-01 10:05:40 -0700

    Core goodness

--- src/lein_test/core.clj
+++ src/lein_test/core.clj
@@ -55,19 +55,17 @@
                 (map #(cstr/replace % #"./action_cache/" ""))
                 (map extract-file-meta)
                 sort-files
-
-                       (take 100)
-
+                (take 100)
                 (map #(json->actions "./action_cache/" (:filename %) (:space %) (:author %)))))
 
 (defn populate-entities
   "Takes in a seq of actions and populates the `entities` table"
   [actions]
   (let [formatted-sql (-> (h/insert-into :public/entities)
-      (h/values (into [] (map ->entity actions)))
-      (h/on-conflict :id (h/do-nothing))
-      (sql/format {:pretty true})
-      ;; try-execute
+                          (h/values (into [] (map ->entity actions)))
+                          (h/on-conflict :id (h/do-nothing))
+                          (sql/format {:pretty true})
+                          try-execute
       )]
     (println "Generated SQL:" formatted-sql)
     formatted-sql
